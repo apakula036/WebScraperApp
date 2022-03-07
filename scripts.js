b5d20c44531686247aa8b24596a29a383b185c66
Andrew <70990567+apakula036@users.noreply.github.com>
2022-03-07 09:12:21 -0800

    Pivoted a bit
    Removed some files and code, condensed into one file, and pivoted ideas a bit.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,45 @@
 
 const puppeteer = require('puppeteer');
 
-autoCheckRocketLeague();
-async function autoCheckRocketLeague(){
+checkStreamerStatus();
+
+async function checkStreamerStatus(){
     const browser = await puppeteer.launch( {headless: false});
     const page = await browser.newPage();
-    await page.goto( "https://www.twitch.tv/tarik");
-    const name = await page.$eval('.CoreText-sc-cpl358-0.joOBZx', el => el.innerHTML)
-    console.log(name + " LIVE")
-    await browser.close();
-};
+    var streamer = "thebausffs"
+    await page.goto( "https://www.twitch.tv/" + streamer )
+    const status = await page.$eval('.CoreText-sc-cpl358-0.joOBZx', el => el.innerHTML)
+    console.log(streamer  + " is " + status)
+    await browser.close()
+}; 
+
+
+/*
+function run () {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto("https://live-tennis.eu/en/atp-live-ranking");
+            let urls = await page.evaluate(() => {
+                let results = [];
+                let items = document.querySelectorAll('td.pn');
+                
+                console.log(items);
+                items.forEach((item) => {
+                    results.push({
+                        url:  item.getAttribute('td'),
+                        text: item.innerText,
+                    });
+                });
+                return results;
+            })
+            browser.close();
+            return resolve(urls);
+        } catch (e) {
+            return reject(e);
+        }
+    })
+}
+run().then(console.log).catch(console.error);
+*/
